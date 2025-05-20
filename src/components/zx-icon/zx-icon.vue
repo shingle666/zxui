ed4f4727615ba225cfe7628b9953893238166cba
tanzhenxing <136670@qq.com>
2025-05-20 21:55:00 +0800

    update

--- src/components/zx-icon/zx-icon.vue
+++ src/components/zx-icon/zx-icon.vue
@@ -1,8 +1,17 @@
 <template>
-	<view @tap="onClick">
-		<image v-if="isImg" :src="name" :mode="imgMode" :style="[imgStyle, customStyle]"></image>
-		<text v-else :style="[iconStyle, customStyle]" :class="iconClass"></text>
-	</view>
+    <image
+      v-if="isImg"
+      :src="name"
+      :mode="mode"
+      :style="imgStyle"
+      @click="onClick"
+    ></image>
+    <text
+      v-else
+      :style="iconStyle"
+      :class="iconClass"
+      @click="onClick"
+    ></text>
 </template>
 
 <script setup>
@@ -14,114 +23,97 @@
  * @property {String}			color			图标颜色
  * @property {String | Number}	size			图标字体大小
  * @property {Boolean}			bold			是否显示粗体
- * @property {String | Number}	index			点击图标的时候传递事件出去的index
  * @property {String}			customPrefix	自定义扩展前缀
- * @property {String}			imgMode			图片的mode
+ * @property {String}			mode			图片的mode
  * @property {String | Number}	width			显示图片小图标时的宽度
  * @property {String | Number}	height			显示图片小图标时的高度
  * @property {String | Number}	top				图标在垂直方向上的定位 用于解决某些情况下，让图标垂直居中的用途
- * @property {Object}			customStyle		icon的样式，对象形式
  * @event {Function}            onClick         点击图标时触发
- * @example 
+ * @example
  * <zx-icon name="eye"></zx-icon>
  */
-import { ref, getCurrentInstance, computed } from 'vue';
-const { proxy } = getCurrentInstance();
+import { computed, defineProps, defineEmits } from "vue";
+
+const emit = defineEmits(["onClick"]);
+
 const props = defineProps({
-	// 图标类名
-	name: {
-		type: String,
-		default: ''
-	},
-	// 图标颜色，可接受主题色
-	color: {
-		type: String,
-		default: '#b0b0b0'
-	},
-	// 字体大小
-	size: {
-		type: [String, Number],
-		default: '34rpx'
-	},
-	// 是否显示粗体
-	bold: {
-		type: Boolean,
-		default: false
-	},
-	// 点击图标的时候传递事件出去的index（用于区分点击了哪一个）
-	index: {
-		type: [String, Number],
-		default: 0
-	},
-	// 自定义扩展前缀，方便用户扩展自己的图标库
-	customPrefix: {
-		type: String,
-		default: 'zx-icon'
-	},
-	// 图片的mode
-	imgMode: {
-		type: String,
-		default: ''
-	},
-	// 用于显示图片小图标时，图片的宽度
-	width: {
-		type: String,
-		default: ''
-	},
-	// 用于显示图片小图标时，图片的高度
-	height: {
-		type: String,
-		default: ''
-	},
-	// 用于解决某些情况下，让图标垂直居中的用途
-	top: {
-		type: String,
-		default: ''
-	},
-	customStyle: {
-		type: Object,
-		default: function() {
-			return {};
-		}
-	}
+  // 图标类名
+  name: {
+    type: String,
+    default: "",
+  },
+  // 图标颜色，可接受主题色
+  color: {
+    type: String,
+    default: "#b0b0b0",
+  },
+  // 字体大小
+  size: {
+    type: [String, Number],
+    default: "34rpx",
+  },
+  // 是否显示粗体
+  bold: {
+    type: Boolean,
+    default: false,
+  },
+  // 自定义扩展前缀，方便用户扩展自己的图标库
+  customPrefix: {
+    type: String,
+    default: "zx-icon",
+  },
+  // 图片的mode
+  mode: {
+    type: String,
+    default: "",
+  },
+  // 用于显示图片小图标时，图片的宽度
+  width: {
+    type: String,
+    default: "",
+  },
+  // 用于显示图片小图标时，图片的高度
+  height: {
+    type: String,
+    default: "",
+  },
+  // 用于解决某些情况下，让图标垂直居中的用途
+  top: {
+    type: String,
+    default: "",
+  },
 });
 
-const iconStyle = computed(() => {
-	let style = {};
-	style = {
-		color: props.color,
-		fontSize: props.size,
-		lineHeight: props.size,
-		fontWeight: props.bold ? 'bold' : 'normal',
-		// 某些特殊情况需要设置一个到顶部的距离，才能更好的垂直居中
-		top: props.top
-	};
-	return style;
-});
-const iconClass = computed(() => {
-	let iconClasses = [];
-	iconClasses.push(props.customPrefix);
-	iconClasses.push(props.customPrefix + '-' + props.name);
-	return iconClasses;
-});
+// 图标样式
+const iconStyle = computed(() => ({
+  color: props.color,
+  fontSize: props.size,
+  lineHeight: props.size,
+  fontWeight: props.bold ? "bold" : "normal",
+  top: props.top,
+}));
+
+// 图标类名
+const iconClass = computed(() => [
+  props.customPrefix,
+  `${props.customPrefix}-${props.name}`,
+]);
+
 // 判断传入的name属性，是否图片路径，只要带有"/"均认为是图片形式
-const isImg = computed(() => {
-	return props.name.indexOf('/') !== -1;
-});
-const imgStyle = computed(() => {
-	let style = {};
-	// 如果设置width和height属性，则优先使用，否则使用size属性
-	style.width = props.width ? props.width : props.size;
-	style.height = props.height ? props.height : props.size;
-	return style;
-});
+const isImg = computed(() => props.name.indexOf("/") !== -1);
 
-const onClick = e => {
-	proxy.$emit('onClick', props.index);
+// 图片样式
+const imgStyle = computed(() => ({
+  width: props.width || props.size,
+  height: props.height || props.size,
+}));
+
+// 点击事件处理
+const onClick = (e) => {
+  emit("onClick", props.index);
 };
 </script>
 
 <style scoped>
-@import './icon.css';
-
+@import "./icon.css";
 </style>
